250599f206a081b0c87bca461cd414cd9a3d8035
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2024-11-20 14:04:51 -0500

    init: skel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}"
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)"
+			},
+			fontFamily: {
+				sans: ["neue-haas-grotesk-text", ...fontFamily.sans],
+				display: ["neue-haas-grotesk-display", ...fontFamily.sans]
+			}
+		}
+	},
+	plugins: []
+} satisfies Config
