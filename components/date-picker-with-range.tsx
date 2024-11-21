fc98d7b3505910b27068fa202b9abfc1287d4f5c
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2024-11-21 17:33:27 -0500

    try to make date picker work on mobile

--- components/date-picker-with-range.tsx
+++ components/date-picker-with-range.tsx
@@ -52,7 +52,17 @@ export default function DatePickerWithRange({
 						)}
 					</Button>
 				</PopoverTrigger>
-				<PopoverContent className="w-auto p-0" align="start">
+				<PopoverContent className="w-auto p-0 md:min-w-[550px]" align="start">
+					<Calendar
+						initialFocus
+						mode="range"
+						defaultMonth={date?.from}
+						selected={date}
+						onSelect={onDateSelect}
+						numberOfMonths={1}
+						className="hidden md:block"
+						disabled={{ before: addDays(new Date(), 0) }}
+					/>
 					<Calendar
 						initialFocus
 						mode="range"
@@ -60,6 +70,7 @@ export default function DatePickerWithRange({
 						selected={date}
 						onSelect={onDateSelect}
 						numberOfMonths={2}
+						className="md:hidden"
 						disabled={{ before: addDays(new Date(), 0) }}
 					/>
 				</PopoverContent>
