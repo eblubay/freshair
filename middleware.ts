ed6f9bed44c901e7aab1145f2af9b7c9841d72a7
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2024-11-21 10:35:06 -0500

    changes

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,12 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
 
 export default clerkMiddleware(async (auth, req) => {
-	if (req.url.includes("/api/apify-webhook")) return
-
 	if (isProtectedRoute(req)) await auth.protect()
 })
 
 export const config = {
 	matcher: [
 		// Skip Next.js internals and all static files, unless found in search params
-		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-		// Always run for API routes
-		"/(api|trpc)(.*)"
+		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)"
 	]
 }
